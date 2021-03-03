035a9c4d69533c898086ee44c2d26d4652f795de
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-03-03 19:52:48 +0200

    Create movie-app strcuture

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../elements/Header/Header";
+import Home from "../home/Home";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Home />
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return <h2>Home Page</h2>;
+  }
+}
+
+export default Home;
